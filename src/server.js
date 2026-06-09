bf77245bef39ac13fbe9d6eedad15d14a19d1762
Gonzalo Rodriguez <gonzalo@codelosophy.com>
2026-06-09 20:22:21 -0300

    Agrego cors a server

--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
 import express from "express";
 import fs from "fs"
+import morgan from "morgan";
+import cors from "cors";
 import { EspecialidadesRoutes } from "./routes/v1/especialidadesV1.routes.js";
 import { MedicosRoutes } from "./routes/v1/medicosV1.routes.js";
 import { ObrasSocialesRoutes } from "./routes/v1/obrasSocialesV1.routes.js";
 import { PacientesRoutes } from "./routes/v1/pacientesV1.routes.js";
 import { RegistroRoutes } from "./routes/v1/registroV1.routes.js";
 import { TurnosRoutes } from "./routes/v1/turnosV1.routes.js";
-import morgan from "morgan";
 
 
 export class Server {
@@ -19,6 +20,7 @@ export class Server {
   }
 
   middlewares() {
+    this.app.use(cors());
     this.app.use(express.json());
     this.app.use("/uploads", express.static("uploads"));
   }
